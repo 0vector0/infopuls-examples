c0b92401e131016cf63d59ae85527fa92171b954
0vector0 <0vector0@gmail.com>
2016-01-12 01:00:29 +0200

    keyboard test

--- src/course01/calculatorTest/ActionTest.java
+++ src/course01/calculatorTest/ActionTest.java
@@ -24,22 +24,36 @@ public class ActionTest {
  * Фрейм с панелью, демонстрирующей действия изменения цвета.
  */
 class ActionFrame extends JFrame {
+
+	private JPanel buttonPanel;
+	private JTextPane textPane;
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+
 	public ActionFrame() {
 		setTitle("ActionTest");
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		buttonPanel = new JPanel();
 		// Определение действий.
-		Action yellowAction = new ColorAction("Yellow", new ImageIcon("yellow-ball.gif"), Color.YELLOW);
-		Action blueAction = new ColorAction("Blue", new ImageIcon("blue-ball.gif"), Color.BLUE);
-		Action redAction = new ColorAction("Red", new ImageIcon("red-ball.gif"), Color.RED);
-		Action button1Action = new ButtonAction("1");
-		
+		// Action button1Action = new ButtonAction("1");
+
+		Action button1Action = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textPane.setText("1");
+			}
+		};
+		Action button2Action = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textPane.setText("2");
+			}
+		};
+
 		// Добавление кнопок для действий.
-		buttonPanel.add(new JButton(yellowAction));
-		buttonPanel.add(new JButton(blueAction));
-		buttonPanel.add(new JButton(redAction));
-		buttonPanel.add(new JButton(button1Action));
-		
+		buttonPanel.add(new JButton("1"));
+		buttonPanel.add(new JButton("2"));
+
 		textPane = new JTextPane();
 		textPane.setText("test");
 		buttonPanel.add(textPane);
@@ -47,57 +61,12 @@ class ActionFrame extends JFrame {
 		add(buttonPanel);
 		// Связывание клавиш Y, B и R с именами.
 		InputMap imap = buttonPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		imap.put(KeyStroke.getKeyStroke("ctrl Y"), "panel.yellow");
-		imap.put(KeyStroke.getKeyStroke("ctrl B"), "panel.blue");
-		imap.put(KeyStroke.getKeyStroke("ctrl R"), "panel.red");
-		imap.put(KeyStroke.getKeyStroke("1"), "panel.1");
+		imap.put(KeyStroke.getKeyStroke("1"), "panel1");
+		imap.put(KeyStroke.getKeyStroke("2"), "panel2");
 		// Связывание имен с действиями.
 		ActionMap amap = buttonPanel.getActionMap();
-		amap.put("panel.yellow", yellowAction);
-		amap.put("panel.blue", blueAction);
-		amap.put("panel.red", redAction);
-		amap.put("panel.1", button1Action);
+		amap.put("panel1", button1Action);
+		amap.put("panel2", button2Action);
 	}
 
-	public class ButtonAction extends AbstractAction{
-		public ButtonAction(String name){
-			putValue(Action.NAME, name);
-		}
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			textPane.setText("1");
-			// TODO Auto-generated method stub
-			
-		}
-	}
-
-	public class ColorAction extends AbstractAction {
-		/**
-		 * Создание действия для изменения цвета.
-		 * 
-		 * @param name
-		 *            Надпись на кнопке
-		 * @param icon
-		 *            Пиктограмма на кнопке
-		 * @param c
-		 *            Цвет фона
-		 */
-		public ColorAction(String name, Icon icon, Color c) {
-			putValue(Action.NAME, name);
-			putValue(Action.SMALL_ICON, icon);
-			putValue(Action.SHORT_DESCRIPTION, "Set panel color to " + name.toLowerCase());
-			putValue("color", c);
-		}
-
-		public void actionPerformed(ActionEvent event) {
-			Color c = (Color) getValue("color");
-			buttonPanel.setBackground(c);
-		}
-	}
-
-	private JPanel buttonPanel;
-	private JTextPane textPane;
-	public static final int DEFAULT_WIDTH = 300;
-	public static final int DEFAULT_HEIGHT = 200;
 }
