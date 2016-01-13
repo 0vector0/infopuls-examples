80eb4527ab45fe9d46e744f3547afb24932d5599
0vector0 <0vector0@gmail.com>
2016-01-13 22:42:53 +0200

    add prj18

---
+++ src/course01/prj18/test.java
@@ -0,0 +1,16 @@
+package course01.prj18;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("test");
+		String s2 = new String("test");
+		
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		Integer int1 = new Integer(1);
+
+	}
+
+}
