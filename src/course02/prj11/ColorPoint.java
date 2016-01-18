faa12f2607c6a479cf63341cae1161ad4a995bcc
0vector0 <0vector0@gmail.com>
2016-01-18 21:23:44 +0200

    add course02

---
+++ src/course02/prj11/ColorPoint.java
@@ -0,0 +1,12 @@
+package course02.prj11;
+
+public class ColorPoint extends Point {
+	int color;
+
+	public ColorPoint(int x, int y, int color) {
+		super(x, y);
+		this.color = color;
+		// TODO Auto-generated constructor stub
+	}
+
+}
