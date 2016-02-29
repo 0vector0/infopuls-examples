b8d65a75b9502c9b0b54753a46e34cbf85e4b6d4
0vector0 <0vector0@gmail.com>
2016-02-29 21:23:19 +0200

    add ArrayList<String>>

--- src/course02/prj23Coll/Main.java
+++ src/course02/prj23Coll/Main.java
@@ -13,8 +13,6 @@ public class Main {
 
 		System.out.println(soccerTeam.players.toString());
 
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- src/course02/prj23Coll/SoccerTeam.java
+++ src/course02/prj23Coll/SoccerTeam.java
@@ -1,21 +1,32 @@
 package course02.prj23Coll;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SoccerTeam {
 	// мапа players. Ключ - номер у команді. Значення - прізвище ігрока.
-	HashMap<String, String> players;
+	HashMap<String, ArrayList<String>> players;
 
 	public SoccerTeam() {
-		players = new HashMap<String, String>();
+		players = new HashMap<String, ArrayList<String>>();
 	}
 
 	public String get(String position) {
-		String name = players.get(position);
-		return name;
+		ArrayList<String> names = players.get(position);
+		return names.toString();
 	}
 
 	public void put(String position, String name) {
-		players.put(position, name);
+
+		ArrayList<String> names = players.get(position);
+
+		if (names == null) {
+			names = new ArrayList<>();
+		}
+
+		names.add(name);
+		players.put(position, names);
+
 	}
+
 }
